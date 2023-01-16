9b27e0de07cf5ab8ad89022be2591356dc0233b1
nguyenhoainam <nguyenhoaiinam2003@gmail.com>
2023-01-16 23:41:31 +0700

    update

--- Coding-challenge/Section10/S10-codingchallage2.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    const header = document.querySelector('h1');
-    header.style.color = 'red';
-  
-    document.querySelector('body').addEventListener('click', function () {
-      header.style.color = 'blue';
-    });
-  })();
